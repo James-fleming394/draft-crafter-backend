e26d3ed2a42a09c228300457e213365fe9601b59
james-fleming394 <james.fleming394@gmail.com>
2024-02-21 16:56:51 -0500

    Draft Route connection to index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const config = require('./config');
 const playerRoutes = require('./routes/playerRoutes');
 const teamRoutes = require('./routes/teamRoutes');
+const draftRoutes = require('./routes/draftRoutes');
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/players', playerRoutes);
 app.use('/api/teams', teamRoutes);
+app.use('/api/drafts', draftRoutes);
 
 // Express middleware and routes will go here
 
